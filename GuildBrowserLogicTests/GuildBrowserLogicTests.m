8140cd70ffd119ddb1e2bf78d7c84d958cd00419
jokerjhm <jokerjhm@msn.com>
2014-05-07 17:45:08 +0800

    加测试文件

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.m
@@ -0,0 +1,68 @@
+//
+//  GuildBrowserLogicTests.m
+//  GuildBrowserLogicTests
+//
+//  Created by 季 浩勉 on 14-5-7.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "WowUtils.h"
+
+@interface GuildBrowserLogicTests : SenTestCase
+
+@end
+
+@implementation GuildBrowserLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+// 1
+-(void)testCharacterClassNameLookup
+{
+    // 2
+    STAssertEqualObjects(@"Warrior",
+                         [WoWUtils classFromCharacterType:1],
+                         @"ClassType should be Warrior");
+    // 3
+    STAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]],
+                  nil);
+    
+    // 4
+    STAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]],
+                 nil);
+    // add the rest as an exercise
+}
+
+- (void)testRaceTypeLookup
+{
+    STAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1], nil);
+    STAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2], nil);
+    STAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]],nil);
+    // add the rest as an exercise
+}
+
+- (void)testQualityLookup
+{
+    STAssertEquals(@"Grey", [WoWUtils qualityFromQualityType:1], nil);
+    STAssertFalse([@"Purple" isEqualToString:[WoWUtils qualityFromQualityType:10]],nil);
+    // add the rest as an exercise
+}
+
+
+@end
